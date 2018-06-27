516e619d18cd728bf124bb43fe47c1d0f211dea9
fananchong <fananchong@126.com>
2018-06-27 21:29:23 +0800

    init

---
+++ src/app/pages/room.controller.js
@@ -0,0 +1,13 @@
+(function () {
+    'use strict';
+
+    module.exports = PageRoom;
+
+    function PageRoom() { }
+
+    PageRoom.onController = function ($scope, $http, user) {
+        $scope.click = function () {
+        };
+    };
+
+})();
